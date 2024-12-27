703a78f03c959109fcaf7c279bd7ce5a9242cc66
Zikra <133850511+AzzikraPraqastaKusuma123@users.noreply.github.com>
2024-12-27 20:53:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,31 @@ const thumbnail = document.querySelector('.thumbnail');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 
-
 if (next && list && thumbnail && slider) {
     next.addEventListener('click', () => {
         initSlider('next');
+    });
+
+    prev.addEventListener('click', () => {
+        initSlider('prev');
     });
 
     const initSlider = (type) => {
         const sliderItems = list.querySelectorAll('.item'); 
         const thumbnailItems = thumbnail.querySelectorAll('.item');
 
-        if (type === 'next' && sliderItems.length > 0 && thumbnailItems.length > 0) {
+        if (type === 'next') {
             list.appendChild(sliderItems[0]); 
             thumbnail.appendChild(thumbnailItems[0]);
-            slider.classList.add('next');
+        } else if (type === 'prev') {
+            list.prepend(sliderItems[sliderItems.length - 1]); 
+            thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
         }
 
+        slider.classList.add(type);
+
         setTimeout(() => {
-            slider.classList.remove('next');
+            slider.classList.remove(type);
         }, 2000);
     };
 }
